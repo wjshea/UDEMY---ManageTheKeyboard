00b6fa786cfa769d98db2057bfb291eb2ab0f383
Bill Shea <billshea@Williams-MBP-2.home>
2014-12-29 21:14:24 -0500

    Initial Commit

---
+++ ManageTheKeyboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ManageTheKeyboard
+//
+//  Created by Bill Shea on 12/29/14.
+//  Copyright (c) 2014 Bill Shea. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
